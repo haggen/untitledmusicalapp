69a7f5dd06acb89cec94b6137af37439b4e48f81
Arthur <arthur@corenzan.com>
2023-05-28 14:58:05 -0300

    Finished draft of synthetsizer

--- src/lib/synthesizer.ts
+++ src/lib/synthesizer.ts
@@ -1,53 +1,123 @@
+/**
+ * Pitch classes and their respective offset from the reference pitch (A) in semitones.
+ * @see https://en.wikipedia.org/wiki/Pitch_(music)
+ */
+export enum Pitch {
+  C = -9,
+  D = -7,
+  E = -5,
+  F = -4,
+  G = -2,
+  A = 0,
+  B = 2,
+}
+
+/**
+ * Accidentals and their respective offset in semitones.
+ * @see https://en.wikipedia.org/wiki/Accidental_(music)
+ */
+export enum Accidental {
+  Flat = -1,
+  Natural = 0,
+  Sharp = 1,
+}
+
+/**
+ * A note specification.
+ */
+type Note = {
+  octave: number;
+  pitch: Pitch;
+  accidental: Accidental;
+};
+
+/**
+ * A "Playable" is something that has a start time and a duration.
+ */
+type Playable = {
+  currentTime?: number;
+  length: number;
+};
+
+/**
+ * Global audio context.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
+ */
 const audioContext = new AudioContext();
 
-export function playSweep() {
+/**
+ * The twelfth root of two.
+ * @see https://en.wikipedia.org/wiki/Equal_temperament
+ */
+const R = 1.059463094359;
+
+/**
+ * The reference note: A4 (A above middle C).
+ */
+const A4 = {
+  octave: 4,
+  pitch: Pitch.A,
+  accidental: Accidental.Natural,
+  frequency: 440,
+};
+
+/**
+ * Calculate semitones between two notes.
+ */
+function getSemitonesBetween(a: Note, b: Note) {
+  return (
+    (a.octave - b.octave) * 12 +
+    (a.accidental + b.accidental) +
+    (a.pitch + b.pitch)
+  );
+}
+
+/**
+ * Calculate frequency of a note in hertz.
+ */
+function getFrequency(note: Note) {
+  return A4.frequency * Math.pow(R, getSemitonesBetween(note, A4));
+}
+
+/**
+ * Play a playable and return the ending time.
+ */
+export async function play(playable: Playable | (Playable & Note)) {
   if (audioContext.state === "suspended") {
-    audioContext.resume();
+    await audioContext.resume();
   }
 
-  const time = audioContext.currentTime;
+  const { currentTime = audioContext.currentTime, length } = playable;
 
-  const oscillatorNode1 = new OscillatorNode(audioContext, {
-    frequency: 220,
-    type: "sine",
-  });
+  if ("pitch" in playable) {
+    const { pitch, octave, accidental = Accidental.Natural } = playable;
 
-  // const oscillatorNode2 = new OscillatorNode(audioContext, {
-  //   frequency: 659.25,
-  //   type: "sine",
-  // });
+    const oscillatorNode = new OscillatorNode(audioContext, {
+      frequency: getFrequency({ octave, pitch, accidental }),
+      type: "sine",
+    });
 
-  const gainNode1 = new GainNode(audioContext);
-  gainNode1.gain.cancelScheduledValues(time);
-  gainNode1.gain.setValueAtTime(1, time);
+    console.log({
+      octave,
+      pitch,
+      accidental,
+      frequency: oscillatorNode.frequency.value,
+    });
 
-  const length = 5;
-  const pulses = 16;
-  const wholestep = length / pulses;
-  const halfstep = wholestep / 2;
+    const gainNode = new GainNode(audioContext);
 
-  // for (let i = 0; i < pulses; i += 1) {
-  //   gainNode1.gain.linearRampToValueAtTime(1, time + wholestep * i + halfstep);
-  //   gainNode1.gain.linearRampToValueAtTime(0, time + wholestep * i + wholestep);
-  // }
+    // Attack and release for 10% of the note's length.
+    gainNode.gain.setValueCurveAtTime(
+      [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      currentTime,
+      length
+    );
 
-  oscillatorNode1.frequency.setValueCurveAtTime(
-    [220, 30, 60, 30, 60, 30, 60, 30, 220],
-    time,
-    length
-  );
+    oscillatorNode.connect(gainNode).connect(audioContext.destination);
 
-  // const gainNode2 = new GainNode(audioContext);
-  // gainNode2.gain.cancelScheduledValues(time);
-  // gainNode2.gain.setValueAtTime(0, time);
-  // gainNode2.gain.setValueAtTime(0, time + 1.5);
-  // gainNode2.gain.linearRampToValueAtTime(1, time + 2);
+    oscillatorNode.start(currentTime);
+    oscillatorNode.stop(currentTime + length);
+  }
 
-  oscillatorNode1.connect(gainNode1).connect(audioContext.destination);
-  oscillatorNode1.start(time);
-  oscillatorNode1.stop(time + length);
-
-  // oscillatorNode2.connect(gainNode2).connect(audioContext.destination);
-  // oscillatorNode2.start(time + 1);
-  // oscillatorNode2.stop(time + 2);
+  return currentTime + length;
 }
